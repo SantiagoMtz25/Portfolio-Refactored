4caf3785c2e71e6ddfbb20a92ba3fbf65d40293b
SantiagoMtz25 <santimtzv01@gmail.com>
2024-08-19 19:44:33 -0600

    final final commit

--- src/components/contact/ContactMe.tsx
+++ src/components/contact/ContactMe.tsx
@@ -1,27 +1,53 @@
-import './ContactMe.css'
-import { useState } from 'react'
+import "./ContactMe.css";
+import { useState } from "react";
+import axios from "axios";
+
+interface SubmitContactForm {
+  name: string;
+  email: string;
+  motive: string | null;
+  message: string | null;
+}
 
 const ContactMe = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [contactNo, setContactNo] = useState<string | null>("");
+  const [motive, setMotive] = useState<string | null>("");
   const [message, setMessage] = useState<string | null>("");
-  
-  const handleSubmit = () => {
-  
+  const [disabledSubmit, setDisabledSubmit] = useState<boolean>(false);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setDisabledSubmit(true);
+    try {
+      const serverUrl = import.meta.env.VITE_API_URL;
+      const response = await axios.post<SubmitContactForm>(`${serverUrl}/`, {
+        name,
+        email,
+        motive,
+        message,
+      });
+      console.log(response.data);
+      clearForm();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      clearForm();
+      setDisabledSubmit(false);
+    }
   };
 
   const clearForm = () => {
     setName("");
     setEmail("");
-    setContactNo("");
+    setMotive("");
     setMessage("");
   };
 
   return (
     <>
       <div className="background">
-        <img className='wave2' src='src\assets\wave (6).svg' alt='wave' />
+        <img className="wave2" src="src\assets\wave (6).svg" alt="wave" />
         <div className="container">
           <div className="screen">
             <div className="screen-header">
@@ -42,7 +68,9 @@ const ContactMe = () => {
                   <span>CONTACT</span>
                   <span>ME</span>
                 </div>
-                <div className="app-contact">CONTACT INFO : santimtzv01@gmail.com</div>
+                <div className="app-contact">
+                  CONTACT INFO : santimtzv01@gmail.com
+                </div>
               </div>
               <div className="screen-body-item">
                 <form className="app-form" onSubmit={handleSubmit} action="">
@@ -72,8 +100,8 @@ const ContactMe = () => {
                       placeholder="CONTACT MOTIVE"
                       required
                       type="text"
-                      value={contactNo || ''}
-                      onChange={(e) => setContactNo(e.target.value)}
+                      value={motive || ""}
+                      onChange={(e) => setMotive(e.target.value)}
                     />
                   </div>
                   <div className="app-form-group message">
@@ -82,23 +110,27 @@ const ContactMe = () => {
                       placeholder="MESSAGE"
                       required
                       type="text"
-                      value={message || ''}
+                      value={message || ""}
                       onChange={(e) => setMessage(e.target.value)}
                     />
                   </div>
                   <div className="app-form-group buttons">
-                    <button className="app-form-button" onClick={clearForm}>CLEAR</button>
-                    <button className="app-form-button" type="submit">SEND</button>
+                    <button className="app-form-button" onClick={clearForm}>
+                      CLEAR
+                    </button>
+                    <button className="app-form-button" type="submit" disabled={disabledSubmit}>
+                      SEND
+                    </button>
                   </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <img className='wave3' src='src\assets\wave (5).svg' alt='wave' />
+        <img className="wave3" src="src\assets\wave (5).svg" alt="wave" />
       </div>
     </>
   );
-}
+};
 
 export default ContactMe;
